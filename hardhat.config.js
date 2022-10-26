a13f91bc91af1bc2f476964fa415ac5844f79310
fps <anthony.nnaemeka.umeh@gmail.com>
2022-10-26 09:06:19 +0100

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+// Install this guy
+// >> npm install @nomiclabs/hardhat-waffle
+require("@nomiclabs/hardhat-waffle");
+module.exports = {
+  solidity: "0.8.17",
+};
